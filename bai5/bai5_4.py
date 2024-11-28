7a9279c518f77c5914ad0d3baa812e7cea05bbf8
Nguyenhai64k2 <ngh79705@gmail.com>
2024-11-28 14:35:18 +0700

    Create bai5_4.py

---
+++ bai5/bai5_4.py
@@ -0,0 +1,10 @@
+print ("sinh vien Nguyen Dinh HAi ")
+print ("mssv 235752021610028")
+import numpy as np
+# Tạo mảng với các giá trị từ 12 đến 38
+array = np.arange(12, 39)
+# Đảo ngược mảng
+reversed_array = array[::-1]
+# In kết quả
+print("Mảng ban đầu:", array)
+print("Mảng đảo ngược:", reversed_array)
