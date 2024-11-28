256e09b144609be0f11421f37ddf75f5010c05cb
Nguyenhai64k2 <ngh79705@gmail.com>
2024-11-28 14:29:38 +0700

    Create bai2_11.py

---
+++ bai2/bai2_11.py
@@ -0,0 +1,10 @@
+print("sinh vien Nguyen Dinh Hai")
+print("mssv 235752021610028")
+l=[1,'python',4,7]
+k=['cse',2,'guntur',8]
+m=[]
+m.append(l);
+m.append(k);
+print(m)
+d={1:1,2:k,'ket hop_danh sach':m}
+print(d)
