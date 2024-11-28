23690795ef3fb50b85bee9735be8f2513a430a64
Nguyenhai64k2 <ngh79705@gmail.com>
2024-11-28 14:23:50 +0700

    Create bai2_1.py

---
+++ bai2/bai2_1.py
@@ -0,0 +1,9 @@
+print("sinh vien Nguyen Dinh Hai")
+print("mssv 235752021610028")
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+if n1>n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
+  
